21b9393d56f805d9bc477018aa1014a995615095
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-02-11 18:50:57 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+namespace View {
+    //% block
+    export function RenderFake3d() {
+        let zLayer = 0
+        const buf = Buffer.create(120)
+        scene.createRenderable(zLayer, (image: Image, camera: scene.Camera) => {
+            for (let x = 0; x < 160; x++) {
+                // Read the current screen content for modification
+                image.getRows(x, buf)
+                // Now "buf" contains a color value for the current pixel row 
+                // (it's actually a vertical column onscreen) where it can be modified.
+                for (let y = 0; y < 120; y++) {
+                    buf[y] = image.getPixel(x, y * 2 - 120)
+                }
+                // Write the modified pixels back to the screen.
+                image.setRows(x, buf)
+            }
+        })
+    }
+}
